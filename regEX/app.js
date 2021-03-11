a43d30493240a6c65411770e61209ab37202650a
DavidHJones36 <43620745+DavidHJones36@users.noreply.github.com>
2021-03-11 16:54:12 -0600

    v7.0

--- regEX/app.js
+++ regEX/app.js
@@ -1,10 +1,44 @@
-let re;
-re = /hello/;
+document.getElementById('name').addEventListener('blur', validateName);
+document.getElementById('zip').addEventListener('blur', validateZip);
+document.getElementById('email').addEventListener('blur', validateEmail);
+document.getElementById('phone').addEventListener('blur', validatePhone);
 
-// console.log(re);
-// console.log(re.source);
+function validateName() {
+  const name = document.getElementById('name');
+  const re = /^[a-zA-Z]{2,10}$/;
+  if (!re.test(name.value)) {
+    name.classList.add('is-invalid');
+  } else {
+    name.classList.remove('is-invalid');
+  }
+}
 
-//exec
+function validateZip() {
+  const zip = document.getElementById('zip');
+  const re = /^[0-9]{5}(-[0-9]{4})?$/;
+  if (!re.test(zip.value)) {
+    zip.classList.add('is-invalid');
+  } else {
+    zip.classList.remove('is-invalid');
+  }
+}
 
-const result = re.exec('hello world');
-console.log(result);
+function validateEmail() {
+  const email = document.getElementById('email');
+  const re = /^([a-zA-z0-9_\-\.]+)@([a-zA-z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+  if (!re.test(email.value)) {
+    email.classList.add('is-invalid');
+  } else {
+    email.classList.remove('is-invalid');
+  }
+}
+
+function validatePhone() {
+  const phone = document.getElementById('phone');
+  const re = /^\(?\d{3}\)?[-. ]?\d{3}[-. ]?\d{4}$/;
+  if (!re.test(phone.value)) {
+    phone.classList.add('is-invalid');
+  } else {
+    phone.classList.remove('is-invalid');
+  }
+}
---
+++ regEX/app_old.js
@@ -0,0 +1,31 @@
+// let re;
+// re = /hello/
+// re = /hello/i; //i = case insensitive
+// re = /hello/g // Global search (all instances)
+
+// console.log(re);
+// console.log(re.source);
+
+//exec
+// const result = re.exec('hell world');
+// console.log(result);
+// console.log(result[0]);
+// console.log(result.index);
+// console.log(result.input);
+
+//test
+// const result = re.test('Hello');
+
+//match
+// const str = 'hello there';
+// const result = str.match(re);
+
+//search - returns index or -1 if not found
+// const str = "there hell";
+// const result = str.search(re);
+
+//replace
+// const str = 'there hello';
+// const result = str.replace(re, 'hi');
+
+// console.log(result);
